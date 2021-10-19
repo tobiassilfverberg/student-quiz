dd5587d7a15f71ae6e4c254eb04de953269d901a
tobiassilfverberg <tobiassilfverberg@gmail.com>
2021-10-19 12:00:45 +0200

    asd

---
+++ 01-basics/script.js
@@ -0,0 +1,33 @@
+/* shift + option + A för att kommentera markerad kod på mac, shift + alt + A på windows */
+
+//Skapa och deklarera en variabel
+/* let msg;
+msg = "Hejsan kompis";
+msg = "Tjenare mannen";
+
+//alert(msg);
+console.log(msg);
+
+msg = "Hallå din bluffare";
+console.log(msg);
+
+msg = "Hej på dig";
+console.log(msg); */
+
+/* variabel-namn inleds med liten bokstav: snake-case eller camelCase
+klassnamn inleds med stor bokstav: PascalCase/UpperCamelCase
+let firstName; //camelCase
+let FirstName; //PascalCase/UpperCamelCase
+let first_name; //snake-case */
+
+let firstName = "Tobias";
+let lastName = "Silfverberg";
+
+let helloMsg = ("Hello " + firstName + " " + lastName);
+console.log(helloMsg);
+
+var dontUseVar = "Använd inte var!"
+console.log(dontUseVar);
+
+const age = "20";
+console.log(age);
