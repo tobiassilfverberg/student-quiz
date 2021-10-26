2a8ac9c30d49a64d2787a5a58aa0b4eef7dd095e
tobiassilfverberg <tobiassilfverberg@gmail.com>
2021-10-26 09:21:27 +0200

    hej

--- dag-3/15-guess-the-number/script.js
+++ dag-3/15-guess-the-number/script.js
@@ -1,35 +1,42 @@
-let guesses = 0;
-let highscore;
+let guesses = 0; //låt antalet gissningar vara 0
+let highscore; //initera highscore, behövs inget värde äm
 
-let number = Math.ceil(Math.random() * 10);
-let running = true;
+let number = Math.ceil(Math.random() * 10); //skapa slumpat tal
+let running = true; //så att while-loopen alltid kör
 
 while (running) {
-  let answer = prompt("Gissa på ett tal mellan 1-10");
+  //starta loop
+  let answer = prompt("Gissa på ett tal mellan 1-10"); //be användaren gissa på ett tal 1-10
 
   if (answer == 0) {
-    running = false;
-    break;
+    //om svaret är 0
+    running = false; //loopen ska inte köra mer
+    break; //bryt loopen och stäng ner
   }
 
   if (answer == number) {
-    guesses++;
+    //om gissningen är rätt
+    guesses++; //öka antalet gissningar med 1
 
     alert(
+      //visa meddelande som säger att svaret var rätt, antal gissnignar.
       `Grattis, du gissade rätt. Rätt svar är: ${number}. Antal gissningar: ${guesses}`
     );
-    number = Math.ceil(Math.random() * 10);
-    if (!highscore) highscore = guesses;
+    number = Math.ceil(Math.random() * 10); //slumpa fram nytt tal
+    if (!highscore) highscore = guesses; //om highscore är undefined, sätt den till antal gissningar
     if (guesses < highscore) {
-      highscore = guesses;
-      alert(`Grattis, nytt highscore! Ditt nya highscore är ${highscore}`);
+      //om antal gissngar är färre än highscore
+      highscore = guesses; //sätt highscore till antalet gissningar
+      alert(`Grattis, nytt highscore! Ditt nya highscore är ${highscore}`); //nytt highscore
     }
-    guesses = 0;
-    continue;
+    guesses = 0; //sätt antal gissningar på 0 igen
+    continue; //börja om
   } else if (answer < number) {
+    //annars om svaret är för lågt
     alert("Fel svar, du gissade för lågt. Gissa igen");
   } else if (answer > number) {
+    //annars om svaret är för högt
     alert("Fel svar, du gissade för högt. Gissa igen");
   }
-  guesses++;
+  guesses++; //öka antalet gissningar
 }
