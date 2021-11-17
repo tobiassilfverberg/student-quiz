e8582648ccfaa050448d0a7a9dc9b56ecaf24ce5
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2021-11-17 20:51:13 +0100

    Fixed syntax error on function removeitem

--- student-quiz/app.js
+++ student-quiz/app.js
@@ -175,7 +175,7 @@ let amountOfGuesses = 0;
 function removeItemOnce(array, value) {
 	var index = array.indexOf(value);
 	if (index > -1) {
-	  arr.splice(index, 1);
+	  array.splice(index, 1);
 	}
 	return array;
   }
